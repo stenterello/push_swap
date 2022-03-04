d5e633125b6223e50ea1f000bdadeecc24916e12
stenterello <davide.delladio@gmail.com>
2022-03-04 15:35:10 +0100

    bonus

--- checker.c
+++ checker.c
@@ -21,28 +21,30 @@ int	final_response(t_stack *a, t_stack *b)
 
 void	make_move(char *str, t_stack *a, t_stack *b)
 {
-	if (!ft_strncmp(str, "rrr", 3))
+	if (!ft_strncmp(str, "rrr\n", 4))
 		checker_rrr(a, b);
-	else if (!ft_strncmp(str, "rrb", 3))
+	else if (!ft_strncmp(str, "rrb\n", 4))
 		checker_rrb(b);
-	else if (!ft_strncmp(str, "rra", 3))
+	else if (!ft_strncmp(str, "rra\n", 4))
 		checker_rra(a);
-	else if (!ft_strncmp(str, "rr", 2))
+	else if (!ft_strncmp(str, "rr\n", 3))
 		checker_rr(a, b);
-	else if (!ft_strncmp(str, "rb", 2))
+	else if (!ft_strncmp(str, "rb\n", 3))
 		checker_rb(b);
-	else if (!ft_strncmp(str, "ra", 2))
+	else if (!ft_strncmp(str, "ra\n", 3))
 		checker_ra(a);
-	else if (!ft_strncmp(str, "pb", 2))
+	else if (!ft_strncmp(str, "pb\n", 3))
 		checker_pb(a, b);
-	else if (!ft_strncmp(str, "pa", 2))
+	else if (!ft_strncmp(str, "pa\n", 3))
 		checker_pa(a, b);
-	else if (!ft_strncmp(str, "ss", 2))
+	else if (!ft_strncmp(str, "ss\n", 3))
 		checker_ss(a, b);
-	else if (!ft_strncmp(str, "sb", 2))
+	else if (!ft_strncmp(str, "sb\n", 3))
 		checker_sb(b);
-	else if (!ft_strncmp(str, "sa", 2))
+	else if (!ft_strncmp(str, "sa\n", 3))
 		checker_sa(a);
+	else
+		die("Error");
 }
 
 void	transform(t_stack *a, t_stack *b)
@@ -50,13 +52,16 @@ void	transform(t_stack *a, t_stack *b)
 	char	*str;
 
 	str = get_next_line(0);
-	while (str)
+	if (str)
 	{
-		find_values(a);
-		find_values(b);
-		make_move(str, a, b);
-		free(str);
-		str = get_next_line(0);
+		while (str)
+		{
+			find_values(a);
+			find_values(b);
+			make_move(str, a, b);
+			free(str);
+			str = get_next_line(0);
+		}
 	}
 	free(str);
 }
